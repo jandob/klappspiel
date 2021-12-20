19eb9835f7de2a14f82bfc185a634ba33b0c059b
jandob <janosch.dobler@gmail.com>
2021-12-20 11:49:05 +0000

    synv

--- src/components/Klappspiel.tsx
+++ src/components/Klappspiel.tsx
@@ -1,111 +1,151 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { FunctionComponent } from 'react';
 
-type Klappe = {
-  open: Boolean
+type Hatch = {
+  open: Boolean,
+  value: number
 }
-type KlappenProps = {
-  klappen: Klappe[],
-  die1: number,
-  die2: number,
+type HatchesProps = {
+  hatches: (Hatch & {closeable: Boolean})[],
   onClick: (idx: number) => void
 }
 
-const canBeClosed = (die1: number, die2: number, klappenValue: number) => {
-  if (((die1 + die2) === klappenValue) ||
-     ((die1 * die2) === klappenValue) ||
-     ((die1 / die2) === klappenValue) ||
-     ((die2 / die1) === klappenValue) ||
-     ((die1 - die2) === klappenValue) ||
-     ((die2 - die1) === klappenValue)) 
-  {
-    return [true, true, true]
-  };
-  const die1Usable = die1 === klappenValue
-  const die2Usable = die2 === klappenValue
-  return [die1Usable || die2Usable, die1Usable, die2Usable]
+/*
+const probability = () => {
+  for (var i = 1; i <=6; i++) {
+    for (var j = 1; j <=6; j++) {
+      let [d1Used, d2Used] = [false, false]
+      for (let k = 1; k <=9; k++) {
+        const [closeable, d1, d2] = canBeClosed(i, j, 1) 
+ 
+      }
+
+    }
+  }
+}
+*/
+// Return a list of possible moves, each move closes one or two hatches.
+const allMoves = (die1: number, die2: number) => {
+  const inRange = (v: number) => v > 0 && v < 10 
+  const isWholeNumber = (v: number) => v % 1 === 0 
+  return [
+    [die1 + die2], [die1 * die2],
+    [die1 - die2], [die2 - die1],
+    [die1 / die2], [die2 / die1],
+    [die1, die2]
+  ].filter((v) => v.every(inRange) && v.every(isWholeNumber))
 }
 
-const Klappen: FunctionComponent<KlappenProps> = (props) => {
-  console.log("renderKlappen", props)
-  const klappen = props.klappen
-  const onclick = props.onClick
+// python like range
+const range = (size:number, startAt:number = 0):ReadonlyArray<number> => {
+  return Array.from({length: size}, (x, i) => i + startAt);
+}
+
+const validMoves = (die1: number, die2: number, openHatches: number[]) => {
+  const intersect = <T,>(a: Array<T>, b: Array<T>) => a.filter((x: T) => b.includes(x))
+  return allMoves(die1, die2).filter((move) => {
+    // check that all hatches are open
+    return intersect(move, openHatches).length > 0
+  })
+}
+
+
+const Hatches: FunctionComponent<HatchesProps> = ({hatches, onClick}) => {
   let divs = []
-  for (let i = 0; i < klappen.length; i++) {
-    const text = klappen[i].open ? "offen" : "zu"
-    const [closeable, d1, d2] = canBeClosed(props.die1, props.die2, i+1)
-    const style = klappen[i].open && closeable ? {color: "green"} : {}
-    let klappe = <div key={i} style={style} onClick={() => onclick(i)}>{i+1}: {text}</div>
+  for (let i = 0; i < hatches.length; i++) {
+    const text = hatches[i].open ? "offen" : "zu"
+    const style = hatches[i].closeable ? {color: "green"} : {}
+    let klappe = <div key={i} style={style} onClick={() => onClick(i)}>{i+1}: {text}</div>
     divs.push(klappe)
   }
   return <>{divs}</>;
 } 
 
 export const KlappSpiel = () => {
-  const [die1, setDie1] = useState(1);
-  const [die2, setDie2] = useState(1);
+  const [moveNr, setMoveNr] = useState(1)
+  const [die1, setDie1] = useState({value: 1, hasBeenUsed: false})
+  const [die2, setDie2] = useState({value: 1, hasBeenUsed: false})
   const roll = () => {
-    setDie1(Math.ceil(Math.random() * 6))
-    setDie2(Math.ceil(Math.random() * 6))
+    setDie1({value: Math.ceil(Math.random() * 6), hasBeenUsed: false})
+    setDie2({value: Math.ceil(Math.random() * 6), hasBeenUsed: false})
   }
-  useEffect(roll, [])
-  
-
-  const [klappenPrev, setKlappenPrev] = useState(Array<Klappe>(9).fill({open: true})) 
-  const [klappen, setKlappen] = useState([...klappenPrev]) 
-  const onclick = (idx: number) => {
-    const klappe = klappen[idx]
-    const [closeable, die1Used, die2Used] = canBeClosed(die1, die2, idx + 1)
-    if (klappe.open && closeable) {
-      let newKlappen = [...klappen]
-      newKlappen[idx] = {open: false}
-      setKlappen(newKlappen)
-      if (die1Used) {
-        setDie1(0)
-      }
-      if (die2Used) {
-        setDie2(0)
-      }
-    }
-  }
-
-  const [lost, setLost] = useState(false);
-
   useEffect(() => {
-    let lost = true
-    for (let i = 0; i < klappen.length; i++) {
-      const [closeable, d1, d2] = canBeClosed(die1, die2, i+1)
-      if (klappen[i].open && closeable) {
-        lost = false;
+    console.log("roll")
+    roll()
+  }, []) // roll when mounted
+  
+  const [hatchesState, setHatchesState] = useState(range(9, 1).map(v => ({value: v, open: true})))
+
+  // Compute closable hatches
+  const hatches = useCallback(() => {
+    const openHatches = hatchesState.filter(h => h.open).map(h => h.value)
+    const unique = (v: number, i: number, a: number[]) => a.indexOf(v) === i
+    const closableValues = validMoves(
+      die1.hasBeenUsed ? 0 : die1.value, 
+      die2.hasBeenUsed ? 0 : die2.value, 
+      openHatches
+    ).flat().filter(unique)
+    return hatchesState.map(h => ({...h, closeable: closableValues.includes(h.value)}))
+  }, [hatchesState, die1, die2])
+
+  const lost = useCallback(() => {
+    const someOpen = hatches().some(h => h.open)
+    const canMove = hatches().some(h => h.closeable)
+    const canReroll = die1.hasBeenUsed && die2.hasBeenUsed
+    return !canMove && someOpen && !canReroll
+  }, [hatches, die1, die2])
+
+  // handle click on hatch
+  const handleClick = (idx: number) => {
+    const hatch = hatches()[idx]
+    if (hatch.closeable) {
+      let newHatchesState = [...hatchesState]
+      newHatchesState[idx].open = false
+      setHatchesState(newHatchesState)
+      // mark die as used
+      if (die1.value === hatch.value) {
+        setDie1({...die1, hasBeenUsed: true})
+      }
+      else if (die2.value === hatch.value) {
+        setDie2({...die2, hasBeenUsed: true})
+      } else {
+        setDie1({...die1, hasBeenUsed: true})
+        setDie2({...die2, hasBeenUsed: true})
       }
     }
-    if (die1 === 0 && die2 === 0) {
+  }
+  // Reroll automatically if both dice have been used.
+  useEffect(() => {
+    if (die1.hasBeenUsed && die2.hasBeenUsed && !lost()) {
+      console.log('reroll')
       roll()
-    } else if (lost) {
-      setLost(true)
+      setMoveNr(moveNr + 1)
     }
-    console.log(die1, die2, lost, klappen.map((k, i) => {
-      const [closeable, d1, d2] = canBeClosed(die1, die2, i+1)
-      return closeable
-    }))
-  }, [die1, die2])
+  }, [die1, die2, lost, moveNr])
 
   const reset = () => {
-    setLost(false)
-    setKlappen(klappen.map(() => ({open: true})))
+    let newHatchesState = [...hatchesState]
+    newHatchesState.map(h => h.open = true)
+    setHatchesState(newHatchesState)
     roll()
+    setMoveNr(1)
   }
 
-  const youLost = lost ? <div style={{color: "red"}}>You Lost!</div> : <></>
-
+  const youLost = lost() ? <div style={{color: "red"}}>You Lost!</div> : <></>
+  console.log(hatches())
   return (
     <>
     <h1> Klappspiel Version 0.1</h1>
-    <div> 
-      <Klappen die1={die1} die2={die2} klappen={klappen} onClick={onclick}/>
+    <p>{allMoves(die1.value, die2.value)}</p>
+    <div>
+      <Hatches 
+        hatches={hatches()}
+        onClick={handleClick} 
+      />
     </div>
-    <p>Würfel: {die1} | {die2}</p>
+    <p>Würfel: {die1.hasBeenUsed ? "X" : die1.value} | {die2.hasBeenUsed ? "X" : die2.value}</p>
+    <p>Runde: {moveNr}</p>
     {youLost}
     <button onClick={reset}>Reset</button>
     </>
